bbf51540db0d3d168688b51c3cbb18e45ed45acf
Jason <kallakavumkal@gmail.com>
2014-10-26 19:14:39 -0400

    Final Commit
    Fixed typos. Also changed the cacheSolve function to use an if-else
    instead of the return function.

--- cachematrix.R
+++ cachematrix.R
@@ -10,12 +10,12 @@
 ## is passed one argument x, an invertible matrix (which requires that the matrix be numeric and square).
 makeCacheMatrix <- function(x = matrix()) {
   i <- matrix() #i will store the inverted matrix, inialized to an empty matrix
-  set <- function(y) { #the matrix can be changed using the set object and a matrix arguement y
+  set <- function(y) { #the matrix can be changed using the set object and a matrix argument y
     x <<- y       
     i <<- matrix() #if the matrix is changed, the inverted matrix is set to be empty
   }
   get <- function() x # the get object will return the matrix x
-  setinverse <- function(inverse) i <<- inverse #inverse is set when called from cacheSolve function
+  setinverse <- function(inverse) i <<- inverse #setting the inverse when called from cacheSolve function
   getinverse <- function() i
   list(set = set, get = get,setinverse = setinverse,getinverse = getinverse) #makeCacheMatrix is really a list of functions
 }
@@ -29,11 +29,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) { #the argument x is a special matrix that can store its inverse
   i <- x$getinverse()
-  if(!anyNA(i)) { #if the inverse has been calculated already then notify the user and print the inverse
+  if(!anyNA(i)) { #if the inverse has been calculated already then notify the user and return the inverse
     message("getting cached data")
-    return(i)
+    i
   }
+  else  {    #calculate the inverse and store it in x
   i <- solve(x$get()) #i gets the inverse of X
   x$setinverse(i) #stores the inverse in cache
   i
+  }
 }
